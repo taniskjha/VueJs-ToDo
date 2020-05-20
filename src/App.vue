3d40dd93114b99d073d5aea0a4320f1e22b97ba2
tanisk jha <jhatanishqq@outlook.com>
2020-05-20 10:38:35 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<!-- Tempelate -->
+
+<template>
+  <div id="app">
+   {{msg}}
+  </div>
+</template>
+
+<!-- Script-->
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return {
+      msg:'Hello'
+    }
+  }
+}
+</script>
+
+<!-- styles -->
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
